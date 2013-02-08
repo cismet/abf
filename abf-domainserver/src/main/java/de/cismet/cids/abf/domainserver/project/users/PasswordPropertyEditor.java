7f7299096ef59d296181334b046690dee775f254
mscholl <mscholl@1141b8cd-3b80-46cc-8deb-676568e6de14>
2013-02-08 13:46:50 +0000

    improved property editor implementation so that it sets the new value only if the change was actually accepted

--- abf-domainserver/src/main/java/de/cismet/cids/abf/domainserver/project/users/PasswordPropertyEditor.java
+++ abf-domainserver/src/main/java/de/cismet/cids/abf/domainserver/project/users/PasswordPropertyEditor.java
@@ -32,6 +32,7 @@ public final class PasswordPropertyEditor extends PropertyEditorSupport implemen
     //~ Instance fields --------------------------------------------------------
 
     private final transient DefaultPasswordComponentModel pwModel;
+    private final transient PasswordPanel pwPanel;
 
     // WTH are there two property env instances, and only one of then really does what it is supposed to
     private transient Set<PropertyEnv> env;
@@ -42,8 +43,10 @@ public final class PasswordPropertyEditor extends PropertyEditorSupport implemen
      * Creates a new PasswordPropertyEditor object.
      */
     public PasswordPropertyEditor() {
-        this.pwModel = new DefaultPasswordComponentModel();
-        this.pwModel.addPropertyChangeListener(WeakListeners.propertyChange(this, this.pwModel));
+        pwModel = new DefaultPasswordComponentModel();
+        pwPanel = new PasswordPanel(pwModel);
+
+        pwModel.addPropertyChangeListener(WeakListeners.propertyChange(this, pwModel));
         env = new HashSet<PropertyEnv>();
     }
 
@@ -51,7 +54,7 @@ public final class PasswordPropertyEditor extends PropertyEditorSupport implemen
 
     @Override
     public Component getCustomEditor() {
-        return new PasswordPanel(pwModel);
+        return pwPanel;
     }
 
     @Override
@@ -62,6 +65,7 @@ public final class PasswordPropertyEditor extends PropertyEditorSupport implemen
     @Override
     public void attachEnv(final PropertyEnv pe) {
         this.env.add(pe);
+        pe.addPropertyChangeListener(WeakListeners.propertyChange(this, pe));
     }
 
     @Override
@@ -70,7 +74,8 @@ public final class PasswordPropertyEditor extends PropertyEditorSupport implemen
                     || DefaultPasswordComponentModel.PROP_PW_MATCH.equals(evt.getPropertyName())) {
             final Object state;
             if (pwModel.isValid()) {
-                state = PropertyEnv.STATE_VALID;
+                // never set to valid state, valid state will be set if ok is pressed and no veto was raised
+                state = PropertyEnv.STATE_NEEDS_VALIDATION;
             } else {
                 state = PropertyEnv.STATE_INVALID;
             }
@@ -78,15 +83,25 @@ public final class PasswordPropertyEditor extends PropertyEditorSupport implemen
             for (final PropertyEnv e : env) {
                 e.setState(state);
             }
+        } else if (PropertyEnv.PROP_STATE.equals(evt.getPropertyName())
+                    && (evt.getNewValue() == PropertyEnv.STATE_VALID)) {
+            // We clear the environment at this point as the OK button is pressed and no veto was raised. Thus the
+            // editor is not visualised anymore and the new value is taken over (after set has been called).
+            env.clear();
+            setValue(String.valueOf(pwModel.getPassword()));
         }
     }
 
     @Override
     public Object getValue() {
-        if (pwModel.isValid()) {
-            return String.valueOf(pwModel.getPassword());
-        } else {
-            return null;
-        }
+        // Clear the editor. We have to use this as we don't have any 'editingFinished' hook regardless of actual state
+        // (essentially the information whether OK or Cancel has been pressed). This operation is called before the
+        // editor is visualised and afterwards, too. In case of a 'OK-Button-Pressed' this operation is called right
+        // AFTER set and thus it is safe to clear the panel content. However, as said before it is also called before
+        // and hence it is not a good idea to clean the PropertyEnv list as we will loose the PCL and will never be
+        // informed of the current validity state.
+        pwPanel.clear();
+
+        return super.getValue();
     }
 }
